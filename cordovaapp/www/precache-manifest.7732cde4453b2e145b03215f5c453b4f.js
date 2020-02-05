14e4d0df7cbe7005fabe63d79d6f5a62ca2378fa
Ski <mike@geoskygps.com>
2020-02-05 08:55:39 -0600

    fix: added required scripts and cordova to start off project

---
+++ cordovaapp/www/precache-manifest.7732cde4453b2e145b03215f5c453b4f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7151faea8e79805768091b2c9ffdeda8",
+    "url": "/pristine-typescript-react-material-ui/index.html"
+  },
+  {
+    "revision": "803cb54126d5d9801a8a",
+    "url": "/pristine-typescript-react-material-ui/static/css/main.8455fdcf.chunk.css"
+  },
+  {
+    "revision": "e730763a960056fc98b7",
+    "url": "/pristine-typescript-react-material-ui/static/js/2.bdea3f27.chunk.js"
+  },
+  {
+    "revision": "803cb54126d5d9801a8a",
+    "url": "/pristine-typescript-react-material-ui/static/js/main.db160e3b.chunk.js"
+  },
+  {
+    "revision": "ddae621798e6c0515f3b",
+    "url": "/pristine-typescript-react-material-ui/static/js/runtime~main.d843c283.js"
+  }
+]);
